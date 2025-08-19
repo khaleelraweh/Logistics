31ed17be6cccc167c9a1bd2070f006f5055ed57d
khaleelraweh <khaleelvisa@gmail.com>
2025-08-19 16:40:29 +0300

    save all

--- resources/views/admin/warehouse_rentals/index.blade.php
+++ resources/views/admin/warehouse_rentals/index.blade.php
@@ -102,39 +102,10 @@
                                 <td>
                                     <span class="fw-bold text-success">{{ number_format($rental->price, 2) }} {{ __('general.sar') }}</span>
                                 </td>
-                                {{-- <td>
-                                    <div class="form-check form-switch d-flex justify-content-center">
-                                        <input class="form-check-input update-status" type="checkbox"
-                                               data-id="{{ $rental->id }}"
-                                               {{ $rental->status ? 'checked' : '' }}>
-                                        <label class="form-check-label ms-2">
-                                            {{ $rental->status ? __('panel.status_active') : __('panel.status_inactive') }}
-                                        </label>
-                                    </div>
-                                </td> --}}
 
                                 <td>
                                     <a href="javascript:void(0);"
-                                        class="updateWarehouseRentalStatus d-flex align-items-center form-switch"
-                                        id="warehouse-rental-{{ $rental->id }}"
-                                        warehouse_rental_id="{{ $rental->id }}"
-                                        data-active-text="{{ __('panel.status_active') }}"
-                                        data-inactive-text="{{ __('panel.status_inactive') }}">
-
-                                        @if ($rental->status == 1)
-                                            <i class="fas fa-toggle-on fa-lg text-success" aria-hidden="true" status="Active" style="font-size:1.6em"></i>
-                                            <span class="ms-1 text-success fw-bold">{{ __('panel.status_active') }}</span>
-
-                                        @else
-                                            <i class="fas fa-toggle-off fa-lg text-warning" aria-hidden="true" status="Inactive" style="font-size:1.6em"></i>
-                                            <span class="ms-1 text-warning fw-bold">{{ __('panel.status_inactive') }}</span>
-                                        @endif
-                                    </a>
-                                </td>
-
-                                <td>
-                                    <a href="javascript:void(0);"
-                                        class="updateWarehouseRentalStatus d-flex align-items-center form-switch"
+                                        class="updateWarehouseRentalStatus d-flex align-items-center"
                                         id="warehouse-rental-{{ $rental->id }}"
                                         warehouse_rental_id="{{ $rental->id }}"
                                         data-active-text="{{ __('panel.status_active') }}"
