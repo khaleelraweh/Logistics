63d58819f5b7a9cca2a545dc930148fb82de5ef6
khaleelraweh <khaleelvisa@gmail.com>
2025-08-20 22:17:14 +0300

    save all

--- resources/views/admin/packages/create.blade.php
+++ resources/views/admin/packages/create.blade.php
@@ -423,6 +423,19 @@
                                                 </div>
                                             </div>
                                         </div>
+
+                                        <div class="row">
+                                            <div class="col-lg-12">
+                                                <div class="card">
+                                                    <div class="card-header">
+                                                        <h5 class="mb-0"><i class="fas fa-cube me-2"></i>مواصفات الطرد</h5>
+                                                    </div>
+                                                    <div class="card-body">
+                                                        @livewire('admin.package.create-product-component')
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
                                 <div class="tab-pane" id="confirm-detail">
