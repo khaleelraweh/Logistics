7b2bdbaf8ed5b2c04f370037b816cec9b9d9ff81
khaleelraweh <khaleelvisa@gmail.com>
2025-08-20 22:34:20 +0300

    save all

--- resources/views/admin/packages/create.blade.php
+++ resources/views/admin/packages/create.blade.php
@@ -450,60 +450,60 @@
                                 <div class="tab-pane" id="delivery-options">
                                     <div>
                                         <div class="row">
-                                            <div class="col-lg-12">
+                                            <div class="col-lg-6">
                                                 <div class="card">
                                                     <div class="card-header">
-                                                        <h5 class="mb-0"><i class="fas fa-cube me-2"></i>مواصفات الطرد</h5>
+                                                        <h5 class="mb-0"><i class="fas fa-shipping-fast me-2"></i>خيارات التوصيل</h5>
                                                     </div>
                                                     <div class="card-body">
                                                         <div class="row mb-3">
-                                                            <div class="col-md-4">
-                                                                <label for="package_type" class="form-label">نوع الطرد</label>
-                                                                <select class="form-select" id="package_type" name="package_type">
-                                                                    <option value="box" {{ old('package_type') == 'box' ? 'selected' : '' }}>صندوق</option>
-                                                                    <option value="envelope" {{ old('package_type') == 'envelope' ? 'selected' : '' }}>مظروف</option>
-                                                                    <option value="pallet" {{ old('package_type') == 'pallet' ? 'selected' : '' }}>بالت</option>
-                                                                    <option value="tube" {{ old('package_type') == 'tube' ? 'selected' : '' }}>أنبوب</option>
-                                                                    <option value="bag" {{ old('package_type') == 'bag' ? 'selected' : '' }}>حقيبة</option>
+                                                            <div class="col-md-6">
+                                                                <label for="delivery_speed" class="form-label">سرعة التوصيل</label>
+                                                                <select class="form-select" id="delivery_speed" name="delivery_speed">
+                                                                    <option value="standard" {{ old('delivery_speed') == 'standard' ? 'selected' : '' }}>عادي</option>
+                                                                    <option value="express" {{ old('delivery_speed') == 'express' ? 'selected' : '' }}>سريع</option>
+                                                                    <option value="same_day" {{ old('delivery_speed') == 'same_day' ? 'selected' : '' }}>نفس اليوم</option>
+                                                                    <option value="next_day" {{ old('delivery_speed') == 'next_day' ? 'selected' : '' }}>اليوم التالي</option>
                                                                 </select>
                                                             </div>
-                                                            <div class="col-md-4">
-                                                                <label for="package_size" class="form-label">الحجم</label>
-                                                                <select class="form-select" id="package_size" name="package_size">
-                                                                    <option value="small" {{ old('package_size') == 'small' ? 'selected' : '' }}>صغير</option>
-                                                                    <option value="medium" {{ old('package_size') == 'medium' ? 'selected' : '' }}>متوسط</option>
-                                                                    <option value="large" {{ old('package_size') == 'large' ? 'selected' : '' }}>كبير</option>
-                                                                    <option value="oversized" {{ old('package_size') == 'oversized' ? 'selected' : '' }}>كبير جداً</option>
-                                                                </select>
-                                                            </div>
-                                                            <div class="col-md-4">
-                                                                <label for="weight" class="form-label">الوزن (كجم)</label>
-                                                                <input type="number" step="0.01" class="form-control" id="weight" name="weight" value="{{ old('weight', 0) }}">
-                                                                @error('weight')
-                                                                    <div class="text-danger small mt-1">{{ $message }}</div>
-                                                                @enderror
+                                                            <div class="col-md-6">
+                                                                <label for="delivery_date" class="form-label">تاريخ التوصيل</label>
+                                                                <input type="date" class="form-control" id="delivery_date" name="delivery_date" value="{{ old('delivery_date') }}">
                                                             </div>
                                                         </div>
 
                                                         <div class="row mb-3">
-                                                            <div class="col-md-4">
-                                                                <label for="dimensions.length" class="form-label">الطول (سم)</label>
-                                                                <input type="number" step="0.01" class="form-control" id="dimensions.length" name="dimensions[length]" value="{{ old('dimensions.length', 0) }}">
-                                                                @error('dimensions.length')
-                                                                    <div class="text-danger small mt-1">{{ $message }}</div>
-                                                                @enderror
+                                                            <div class="col-md-6">
+                                                                <label for="delivery_method" class="form-label">طريقة التوصيل</label>
+                                                                <select class="form-select" id="delivery_method" name="delivery_method">
+                                                                    <option value="standard" {{ old('delivery_method') == 'standard' ? 'selected' : '' }}>عادي</option>
+                                                                    <option value="express" {{ old('delivery_method') == 'express' ? 'selected' : '' }}>سريع</option>
+                                                                    <option value="pickup" {{ old('delivery_method') == 'pickup' ? 'selected' : '' }}>استلام من الفرع</option>
+                                                                    <option value="courier" {{ old('delivery_method') == 'courier' ? 'selected' : '' }}>مستعجل</option>
+                                                                </select>
                                                             </div>
-                                                            <div class="col-md-4">
-                                                                <label for="dimensions.width" class="form-label">العرض (سم)</label>
-                                                                <input type="number" step="0.01" class="form-control" id="dimensions.width" name="dimensions[width]" value="{{ old('dimensions.width', 0) }}">
-                                                                @error('dimensions.width')
-                                                                    <div class="text-danger small mt-1">{{ $message }}</div>
-                                                                @enderror
+                                                            <div class="col-md-6">
+                                                                <label for="origin_type" class="form-label">نوع المصدر</label>
+                                                                <select class="form-select" id="origin_type" name="origin_type">
+                                                                    <option value="warehouse" {{ old('origin_type') == 'warehouse' ? 'selected' : '' }}>مستودع</option>
+                                                                    <option value="store" {{ old('origin_type') == 'store' ? 'selected' : '' }}>متجر</option>
+                                                                    <option value="home" {{ old('origin_type') == 'home' ? 'selected' : '' }}>منزل</option>
+                                                                    <option value="other" {{ old('origin_type') == 'other' ? 'selected' : '' }}>أخرى</option>
+                                                                </select>
                                                             </div>
-                                                            <div class="col-md-4">
-                                                                <label for="dimensions.height" class="form-label">الارتفاع (سم)</label>
-                                                                <input type="number" step="0.01" class="form-control" id="dimensions.height" name="dimensions[height]" value="{{ old('dimensions.height', 0) }}">
-                                                                @error('dimensions.height')
+                                                        </div>
+
+                                                        <div class="row mb-3">
+                                                            <div class="col-md-6">
+                                                                <label for="status1" class="form-label">الحالة</label>
+                                                                <select class="form-select" id="status1" name="status">
+                                                                    @foreach (\App\Models\Package::statuses() as $key => $label)
+                                                                        <option value="{{ $key }}" {{ old('status', $package->status ?? '') == $key ? 'selected' : '' }}>
+                                                                            {{ $label }}
+                                                                        </option>
+                                                                    @endforeach
+                                                                </select>
+                                                                @error('status')
                                                                     <div class="text-danger small mt-1">{{ $message }}</div>
                                                                 @enderror
                                                             </div>
@@ -511,36 +511,24 @@
 
                                                         <div class="row mb-3">
                                                             <div class="col-12">
-                                                                <label for="package_content" class="form-label">محتويات الطرد</label>
-                                                                <textarea class="form-control" id="package_content" name="package_content" rows="3">{{ old('package_content') }}</textarea>
-                                                                @error('package_content')
-                                                                    <div class="text-danger small mt-1">{{ $message }}</div>
-                                                                @enderror
-                                                            </div>
-                                                        </div>
-
-                                                        <div class="row mb-3">
-                                                            <div class="col-12">
-                                                                <label for="package_note" class="form-label">ملاحظات</label>
-                                                                <textarea class="form-control" id="package_note" name="package_note" rows="3">{{ old('package_note') }}</textarea>
-                                                                @error('package_note')
-                                                                    <div class="text-danger small mt-1">{{ $message }}</div>
-                                                                @enderror
+                                                                <label for="delivery_status_note" class="form-label">ملاحظات الحالة</label>
+                                                                <textarea class="form-control" id="delivery_status_note" name="delivery_status_note" rows="3">{{ old('delivery_status_note') }}</textarea>
                                                             </div>
                                                         </div>
                                                     </div>
                                                 </div>
                                             </div>
-                                        </div>
-
-                                        <div class="row">
-                                            <div class="col-lg-12">
+                                            <div class="col-lg-6">
                                                 <div class="card">
                                                     <div class="card-header">
-                                                        <h5 class="mb-0"><i class="fas fa-cube me-2"></i>مواصفات الطرد</h5>
+                                                        <h5 class="mb-0"><i class="fas fa-shipping-fast me-2"></i>التحصيل</h5>
                                                     </div>
                                                     <div class="card-body">
-                                                        @livewire('admin.package.create-product-component')
+                                                        <div class="row mb-3">
+                                                            <div class="col-md-12">
+                                                                @livewire('admin.package.create-package-collection-component')
+                                                            </div>
+                                                        </div>
                                                     </div>
                                                 </div>
                                             </div>
