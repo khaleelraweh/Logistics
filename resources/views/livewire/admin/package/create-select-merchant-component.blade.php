36596ce6da7e1183bafe7638487eabdfd547c83c
khaleelraweh <khaleelvisa@gmail.com>
2025-08-23 00:09:44 +0300

    sasve all

--- resources/views/livewire/admin/package/create-select-merchant-component.blade.php
+++ resources/views/livewire/admin/package/create-select-merchant-component.blade.php
@@ -1,6 +1,6 @@
 <div>
-    <div class="row mb-3">
-        <div class="col-12">
+    <div class="row">
+        <div class="col-12 mb-3">
             <label for="merchant_id" class="form-label">{{ __('general.merchant') }} ({{ __('general.optional') }})</label>
             <select class="form-select" wire:model="merchant_id" id="merchant_id" name="merchant_id">
                 <option value="">{{ __('general.select') }}</option>
@@ -11,33 +11,33 @@
         </div>
     </div>
 
-    <div class="row mb-3">
-        <div class="col-md-4">
+    <div class="row">
+        <div class="col-md-4 mb-3">
             <label class="form-label">{{ __('package.sender_first_name') }} <span class="text-danger">*</span></label>
             <input type="text" class="form-control" wire:model="sender_first_name" name="sender_first_name" required>
             @error('sender_first_name') <div class="invalid-feedback">{{ $message }}</div> @enderror
         </div>
 
-        <div class="col-md-4">
+        <div class="col-md-4 mb-3">
             <label class="form-label">{{ __('package.sender_middle_name') }} <span class="text-danger">*</span></label>
             <input type="text" class="form-control" wire:model="sender_middle_name" name="sender_middle_name" required>
             @error('sender_middle_name') <div class="invalid-feedback">{{ $message }}</div> @enderror
         </div>
 
-        <div class="col-md-4">
+        <div class="col-md-4 mb-3">
             <label class="form-label">{{ __('package.sender_last_name') }} <span class="text-danger">*</span></label>
             <input type="text" class="form-control" wire:model="sender_last_name" name="sender_last_name" required>
             @error('sender_last_name') <div class="invalid-feedback">{{ $message }}</div> @enderror
         </div>
     </div>
 
-    <div class="row mb-3">
-        <div class="col-md-6">
+    <div class="row">
+        <div class="col-md-6 mb-3">
             <label class="form-label">{{ __('package.sender_email') }}</label>
             <input type="email" class="form-control" wire:model="sender_email" name="sender_email">
             @error('sender_email') <div class="invalid-feedback">{{ $message }}</div> @enderror
         </div>
-        <div class="col-md-6">
+        <div class="col-md-6 mb-3">
             <label class="form-label">{{ __('package.sender_phone') }} <span class="text-danger">*</span></label>
             <input type="text" class="form-control" wire:model="sender_phone" name="sender_phone" required>
             @error('sender_phone') <div class="invalid-feedback">{{ $message }}</div> @enderror
--- resources/views/livewire/admin/package/create-select-receiver-merchant-component.blade.php
+++ resources/views/livewire/admin/package/create-select-receiver-merchant-component.blade.php
@@ -1,6 +1,6 @@
 <div>
-    <div class="row mb-3">
-        <div class="col-md-12">
+    <div class="row ">
+        <div class="col-md-12 mb-3">
             <label for="receiver_merchant_id" class="form-label">
                 {{ __('general.merchant') }} ({{ __('general.optional') }})
             </label>
@@ -13,33 +13,33 @@
         </div>
     </div>
 
-    <div class="row mb-3">
-        <div class="col-md-4">
+    <div class="row ">
+        <div class="col-md-4 mb-3">
             <label class="form-label">{{ __('package.receiver_first_name') }} <span class="text-danger">*</span></label>
             <input type="text" class="form-control" wire:model="receiver_first_name" name="receiver_first_name" required>
             @error('receiver_first_name') <div class="invalid-feedback">{{ $message }}</div> @enderror
         </div>
 
-        <div class="col-md-4">
+        <div class="col-md-4 mb-3">
             <label class="form-label">{{ __('package.receiver_middle_name') }} <span class="text-danger">*</span></label>
             <input type="text" class="form-control" wire:model="receiver_middle_name" name="receiver_middle_name" required>
             @error('receiver_middle_name') <div class="invalid-feedback">{{ $message }}</div> @enderror
         </div>
 
-        <div class="col-md-4">
+        <div class="col-md-4 mb-3">
             <label class="form-label">{{ __('package.receiver_last_name') }} <span class="text-danger">*</span></label>
             <input type="text" class="form-control" wire:model="receiver_last_name" name="receiver_last_name" required>
             @error('receiver_last_name') <div class="invalid-feedback">{{ $message }}</div> @enderror
         </div>
     </div>
 
-    <div class="row mb-3">
-        <div class="col-md-6">
+    <div class="row ">
+        <div class="col-md-6 mb-3">
             <label class="form-label">{{ __('package.receiver_email') }}</label>
             <input type="email" class="form-control" wire:model="receiver_email" name="receiver_email">
             @error('receiver_email') <div class="invalid-feedback">{{ $message }}</div> @enderror
         </div>
-        <div class="col-md-6">
+        <div class="col-md-6 mb-3">
             <label class="form-label">{{ __('package.receiver_phone') }} <span class="text-danger">*</span></label>
             <input type="text" class="form-control" wire:model="receiver_phone" name="receiver_phone" required>
             @error('receiver_phone') <div class="invalid-feedback">{{ $message }}</div> @enderror
