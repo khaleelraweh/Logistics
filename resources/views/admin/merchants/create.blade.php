ff7db00afc353b6c835279a33c337623a829dc66
khaleelraweh <khaleelvisa@gmail.com>
2025-08-24 17:10:36 +0300

    save all

--- resources/views/admin/merchants/create.blade.php
+++ resources/views/admin/merchants/create.blade.php
@@ -400,21 +400,20 @@
                         </div>
 
                         <!-- Submit Button -->
+                        <div class="text-end pt-3">
+                            @ability('admin', 'create_merchants')
+                                <button type="submit" class="btn btn-primary px-3 d-inline-flex align-items-center">
+                                    <i class="ri-save-3-line me-2"></i>
+                                    <i class="bi bi-save me-2"></i>
+                                    {{ __('merchant.save_merchant_data') }}
+                                </button>
+                            @endability
 
-                           <div class="text-end pt-3">
-                                @ability('admin', 'create_merchants')
-                                    <button type="submit" class="btn btn-primary px-3 d-inline-flex align-items-center">
-                                        <i class="ri-save-3-line me-2"></i>
-                                        <i class="bi bi-save me-2"></i>
-                                        {{ __('merchant.save_merchant_data') }}
-                                    </button>
-                                @endability
-
-                                <a href="{{ route('admin.merchants.index') }}" class="btn btn-outline-danger ms-2">
-                                    <i class="ri-arrow-go-back-line me-1"></i>
-                                    {{ __('panel.cancel') }}
-                                </a>
-                            </div>
+                            <a href="{{ route('admin.merchants.index') }}" class="btn btn-outline-danger ms-2">
+                                <i class="ri-arrow-go-back-line me-1"></i>
+                                {{ __('panel.cancel') }}
+                            </a>
+                        </div>
 
                     </form>
                 </div>
