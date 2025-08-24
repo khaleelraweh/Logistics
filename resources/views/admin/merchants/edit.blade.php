b264e9f9630148d6ca83a239d9144c04bf55b9da
khaleelraweh <khaleelvisa@gmail.com>
2025-08-24 16:37:23 +0300

    save all

--- resources/views/admin/merchants/edit.blade.php
+++ resources/views/admin/merchants/edit.blade.php
@@ -322,20 +322,22 @@
                         </div>
 
                         <!-- Submit Button -->
-                        @ability('admin', 'update_merchants')
+
                            <div class="text-end pt-3">
-                                <button type="submit" class="btn btn-primary  px-3 d-inline-flex align-items-center">
-                                    <i class="ri-save-3-line me-2"></i>
-                                    <i class="bi bi-save me-2"></i>
-                                    {{ __('merchant.update_merchant_data') }}
-                                </button>
+                                @ability('admin', 'update_merchants')
+                                    <button type="submit" class="btn btn-primary  px-3 d-inline-flex align-items-center">
+                                        <i class="ri-save-3-line me-2"></i>
+                                        <i class="bi bi-save me-2"></i>
+                                        {{ __('merchant.update_merchant_data') }}
+                                    </button>
+                                @endability
 
                                 <a href="{{ route('admin.merchants.index') }}" class="btn btn-outline-danger ms-2">
                                     <i class="ri-arrow-go-back-line me-1"></i>
                                     {{ __('panel.cancel') }}
                                 </a>
                             </div>
-                        @endability
+
                     </form>
                 </div>
             </div>
