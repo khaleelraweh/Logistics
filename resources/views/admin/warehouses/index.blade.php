c11d3c8a764ed362348e68d48b5c983a974e0bc6
khaleelraweh <khaleelvisa@gmail.com>
2025-08-25 19:03:31 +0300

    save all

--- resources/views/admin/warehouses/index.blade.php
+++ resources/views/admin/warehouses/index.blade.php
@@ -2,20 +2,25 @@
 
 @section('content')
 
-        <!-- start page title -->
-        <div class="row">
-            <div class="col-12">
-                <div class="page-title-box d-sm-flex align-items-center justify-content-between">
-                    <h4 class="mb-sm-0">{{ __('warehouse.manage_warehouses') }}</h4>
-                    <div class="page-title-right">
+
+    <!-- Page Header -->
+    <div class="row ">
+        <div class="col-12">
+            <div class="page-title-box d-flex align-items-center justify-content-between">
+                <h4 class="mb-0 font-size-18">{{ __('warehouse.manage_warehouses') }}</h4>
+
+                <div class="page-title-right">
+                    <nav aria-label="breadcrumb">
                         <ol class="breadcrumb m-0">
-                            <li class="breadcrumb-item"><a href="javascript:void(0);">{{ __('warehouse.view_warehouses') }}</a></li>
-                            <li class="breadcrumb-item active">{{ __('warehouse.manage_warehouses') }}</li>
+                            <li class="breadcrumb-item"><a href="{{ route('admin.index') }}">{{ __('general.main') }}</a></li>
+                            <li class="breadcrumb-item"><a href="{{ route('admin.warehouses.index') }}">{{ __('warehouse.warehouses') }}</a></li>
                         </ol>
-                    </div>
+                    </nav>
                 </div>
             </div>
         </div>
+    </div>
+    <!-- /Page Header -->
 
         <div class="row">
             <div class="col-12">
