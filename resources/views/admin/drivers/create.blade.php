bb1bd4c3385aa7d8fde95688907fd61e8d6167f3
khaleelraweh <khaleelvisa@gmail.com>
2025-08-25 23:17:40 +0300

    save all

--- resources/views/admin/drivers/create.blade.php
+++ resources/views/admin/drivers/create.blade.php
@@ -2,15 +2,21 @@
 
 @section('content')
 
-    <div class="row">
+
+    <!-- Page Header -->
+    <div class="row ">
         <div class="col-12">
-            <div class="page-title-box d-sm-flex align-items-center justify-content-between">
-                <h4 class="mb-sm-0">{{ __('driver.manage_drivers') }}</h4>
+            <div class="page-title-box d-flex align-items-center justify-content-between">
+                <h4 class="mb-0 font-size-18">{{ __('driver.add_driver') }}</h4>
+
                 <div class="page-title-right">
-                    <ol class="breadcrumb m-0">
-                        <li class="breadcrumb-item"><a href="javascript:void(0);">{{ __('driver.add_driver') }}</a></li>
-                        <li class="breadcrumb-item active">{{ __('driver.manage_drivers') }}</li>
-                    </ol>
+                    <nav aria-label="breadcrumb">
+                        <ol class="breadcrumb m-0">
+                            <li class="breadcrumb-item"><a href="{{ route('admin.index') }}">{{ __('general.main') }}</a></li>
+                            <li class="breadcrumb-item"><a href="{{ route('admin.drivers.index') }}">{{ __('driver.manage_drivers') }}</a></li>
+                            <li class="breadcrumb-item active">{{ __('driver.add_driver') }}</li>
+                        </ol>
+                    </nav>
                 </div>
             </div>
         </div>
