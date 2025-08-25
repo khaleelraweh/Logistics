db0938162c783f5de4f9eb36bf16e495064c4619
khaleelraweh <khaleelvisa@gmail.com>
2025-08-25 22:56:33 +0300

    save all

--- resources/views/admin/drivers/create.blade.php
+++ resources/views/admin/drivers/create.blade.php
@@ -44,7 +44,7 @@
                                 </a>
                             </li>
                             <li class="nav-item">
-                                <a href="#progress-bank-detai2l" class="nav-link" data-toggle="tab">
+                                <a href="#hire-and-supervision" class="nav-link" data-toggle="tab">
                                     <span class="step-number"><i class="mdi mdi-account-supervisor" style="font-size: 1.4rem;"></i></span>
                                     <span class="step-title">{{ __('driver.hire_and_supervision') }}</span>
                                 </a>
@@ -154,6 +154,57 @@
                                 </div>
                             </div>
                             <div class="tab-pane" id="license-and-documentation">
+                                <div>
+                                    <form>
+                                        <div class="row">
+                                            <div class="col-lg-6">
+                                                <div class="mb-3">
+                                                    <label class="form-label" for="progress-basicpill-namecard-input">Name on Card</label>
+                                                    <input type="text" class="form-control" id="progress-basicpill-namecard-input">
+                                                </div>
+                                            </div>
+
+                                            <div class="col-lg-6">
+                                                <div class="mb-3">
+                                                    <label>Credit Card Type</label>
+                                                    <select class="form-select">
+                                                        <option selected>Select Card Type</option>
+                                                        <option value="AE">American Express</option>
+                                                        <option value="VI">Visa</option>
+                                                        <option value="MC">MasterCard</option>
+                                                        <option value="DI">Discover</option>
+                                                    </select>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-lg-6">
+                                                <div class="mb-3">
+                                                    <label class="form-label" for="progress-basicpill-cardno-input">Credit Card Number</label>
+                                                    <input type="text" class="form-control" id="progress-basicpill-cardno-input">
+                                                </div>
+                                            </div>
+
+                                            <div class="col-lg-6">
+                                                <div class="mb-3">
+                                                    <label class="form-label" for="progress-basicpill-card-verification-input">Card Verification Number</label>
+                                                    <input type="text" class="form-control" id="progress-basicpill-card-verification-input">
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-lg-6">
+                                                <div class="mb-3">
+                                                    <label class="form-label" for="progress-basicpill-expiration-input">Expiration Date</label>
+                                                    <input type="text" class="form-control" id="progress-basicpill-expiration-input">
+                                                </div>
+                                            </div>
+
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                            <div class="tab-pane" id="hire-and-supervision">
                                 <div>
                                     <form>
                                         <div class="row">
