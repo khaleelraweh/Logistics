8b5ed1167cf94c8cd6f61dc00a5d5e8ddbb6c572
khaleelraweh <khaleelvisa@gmail.com>
2025-08-26 00:22:49 +0300

    save all

--- resources/views/admin/drivers/create.blade.php
+++ resources/views/admin/drivers/create.blade.php
@@ -314,53 +314,23 @@
                             <!-- step4: Hire and Supervision -->
                             <div class="tab-pane" id="hire-and-supervision">
                                 <div>
-                                    <form>
-                                        <div class="row">
-                                            <div class="col-lg-6">
-                                                <div class="mb-3">
-                                                    <label class="form-label" for="progress-basicpill-namecard-input">Name on Card</label>
-                                                    <input type="text" class="form-control" id="progress-basicpill-namecard-input">
-                                                </div>
-                                            </div>
-
-                                            <div class="col-lg-6">
-                                                <div class="mb-3">
-                                                    <label>Credit Card Type</label>
-                                                    <select class="form-select">
-                                                        <option selected>Select Card Type</option>
-                                                        <option value="AE">American Express</option>
-                                                        <option value="VI">Visa</option>
-                                                        <option value="MC">MasterCard</option>
-                                                        <option value="DI">Discover</option>
-                                                    </select>
-                                                </div>
+                                    <div class="row">
+                                        <div class="col-lg-6">
+                                            <div class="mb-3">
+                                                <label class="form-label" for="username">{{ __('driver.username') }}</label>
+                                                <input type="text" class="form-control" name="username" value="{{ old('username') }}" placeholder="{{ __('driver.username') }}" id="username">
+                                                @error('username')<span class="text-danger">{{ $message }}</span>@enderror
                                             </div>
                                         </div>
-                                        <div class="row">
-                                            <div class="col-lg-6">
-                                                <div class="mb-3">
-                                                    <label class="form-label" for="progress-basicpill-cardno-input">Credit Card Number</label>
-                                                    <input type="text" class="form-control" id="progress-basicpill-cardno-input">
-                                                </div>
-                                            </div>
-
-                                            <div class="col-lg-6">
-                                                <div class="mb-3">
-                                                    <label class="form-label" for="progress-basicpill-card-verification-input">Card Verification Number</label>
-                                                    <input type="text" class="form-control" id="progress-basicpill-card-verification-input">
-                                                </div>
+                                        <div class="col-lg-6">
+                                            <div class="mb-3">
+                                                <label class="form-label" for="password">{{ __('driver.password') }}</label>
+                                                <input type="text" class="form-control" name="password" value="{{ old('password') }}" placeholder="{{ __('driver.password') }}" id="password">
+                                                @error('password')<span class="text-danger">{{ $message }}</span>@enderror
                                             </div>
                                         </div>
-                                        <div class="row">
-                                            <div class="col-lg-6">
-                                                <div class="mb-3">
-                                                    <label class="form-label" for="progress-basicpill-expiration-input">Expiration Date</label>
-                                                    <input type="text" class="form-control" id="progress-basicpill-expiration-input">
-                                                </div>
-                                            </div>
+                                    </div>
 
-                                        </div>
-                                    </form>
                                 </div>
                             </div>
                             <!-- step5: Confirm Details -->
