14474968b00485c61a7bc0a9df28c064fafd4e23
khaleelraweh <khaleelvisa@gmail.com>
2025-08-26 05:55:29 +0300

    save all

--- resources/views/admin/drivers/index.blade.php
+++ resources/views/admin/drivers/index.blade.php
@@ -41,6 +41,10 @@
                     </div>
                 </div>
 
+                <!-- Filters Section -->
+                @include('admin.drivers.filter.filter')
+                <!-- End Filters Section -->
+
                 <table id="datatable" class="table table-bordered dt-responsive nowrap" style="width: 100%;">
                     <thead>
                         <tr>
