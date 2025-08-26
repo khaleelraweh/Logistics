ced7f8cef69242f8e46926160ac40364865bfedd
khaleelraweh <khaleelvisa@gmail.com>
2025-08-26 05:52:11 +0300

    save all

--- app/Models/Driver.php
+++ app/Models/Driver.php
@@ -4,13 +4,60 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Nicolaslopezj\Searchable\SearchableTrait;
 use Spatie\Translatable\HasTranslations;
 
 class Driver extends Model
 {
-    use HasFactory, HasTranslations;
+    use HasFactory, HasTranslations , SearchableTrait;
 
     protected $guarded = [];
+
+
+    protected $searchable = [
+        'columns' => [
+            // كل أعمدة drivers
+            'drivers.first_name'        => 10,
+            'drivers.middle_name'       => 10,
+            'drivers.last_name'     => 10,
+            'drivers.phone'     => 10,
+            'drivers.username'      => 10,
+            'drivers.email'     => 10,
+            'drivers.country'       => 10,
+            'drivers.region'        => 10,
+            'drivers.city'      => 10,
+            'drivers.district'      => 10,
+            'drivers.latitude'      => 10,
+            'drivers.longitude'     => 10,
+            'drivers.license_number'        => 10,
+            'drivers.vehicle_type'      => 10,
+            'drivers.vehicle_number'        => 10,
+            'drivers.vehicle_model'     => 10,
+            'drivers.vehicle_color'     => 10,
+            'drivers.vehicle_capacity_weight'       => 10,
+            'drivers.vehicle_capacity_volume'       => 10,
+            'drivers.license_expiry_date'       => 10,
+            'drivers.hired_date'        => 10,
+            'drivers.supervisor_id'     => 10,
+            'drivers.total_deliveries'      => 10,
+            'drivers.availability_status'       => 10,
+            'drivers.status'        => 10,
+
+
+            // من جدول التجار (المرسل والمستلم)
+            'users.first_name'             => 10,
+            'users.last_name'             => 10,
+            'users.email'             => 10,
+            'users.phone'             => 10,
+
+        ],
+        'joins' => [
+            'users' => ['users.id', 'drivers.supervisor_id'],
+        ],
+    ];
+
+
+
     public $translatable = ['first_name', 'middle_name', 'last_name'];
 
     // to get full name as property instead of first_name and last_name
