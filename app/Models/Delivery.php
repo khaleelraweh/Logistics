22fb29c41a44a2cf85e1f8ebd9bebe4dcca48fff
khaleelraweh <khaleelvisa@gmail.com>
2025-08-26 23:00:24 +0300

    save all

--- app/Models/Delivery.php
+++ app/Models/Delivery.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Nicolaslopezj\Searchable\SearchableTrait;
 
 class Delivery extends Model
 {
-    use HasFactory;
+    use HasFactory , SearchableTrait;
 
 
     protected $guarded = [];
