8428af330ecc3412c67e93366c9fc82229f2abb0
khaleelraweh <khaleelvisa@gmail.com>
2025-08-29 00:04:04 +0300

    save all

--- resources/views/admin/deliveries/show.blade.php
+++ resources/views/admin/deliveries/show.blade.php
@@ -660,3 +660,23 @@
         });
     </script>
 @endsection
+
+
+@php
+ function getStatusColor($status) {
+        return match($status) {
+            'pending'           => 'warning',
+            'assigned_to_driver'=> 'primary',
+            'driver_picked_up'  => 'info',
+            'in_transit'        => 'info',
+            'arrived_at_hub'    => 'secondary',
+            'out_for_delivery'  => 'primary',
+            'delivered'         => 'success',
+            'delivery_failed'   => 'danger',
+            'returned'          => 'secondary',
+            'cancelled'         => 'danger',
+            'in_warehouse'      => 'secondary',
+            default              => 'secondary',
+        };
+    }
+@endphp
