681eff12198a9767a23dea69d7ac645518ae65ca
khaleelraweh <khaleelvisa@gmail.com>
2025-08-29 05:43:30 +0300

    save all

--- resources/views/livewire/admin/return-request/create-return-request-component.blade.php
+++ resources/views/livewire/admin/return-request/create-return-request-component.blade.php
@@ -22,7 +22,7 @@
                         {{ __('return_request.return_request_information') }}
                     </div>
                     <div class="card-body">
-                        <form>
+                        <form wire:submit.prevent="store">
                             <!-- Package Selection -->
                             <div class="row mb-4">
                                 <label class="col-sm-2 col-form-label required-field">{{ __('package.package') }}</label>
