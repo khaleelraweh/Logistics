9ddd414f51f9f0a4dfd0532fa20cb74e84248283
khaleelraweh <khaleelvisa@gmail.com>
2025-09-24 22:57:33 +0300

    save all

--- database/seeders/MerchantPermissionsSeeder.php
+++ database/seeders/MerchantPermissionsSeeder.php
@@ -19,6 +19,23 @@ class MerchantPermissionsSeeder extends Seeder
             ]
         );
 
+         //===== Dashboard =====
+        $manageMain = Permission::create(['name' => 'main', 'display_name' => ['ar' => 'الرئيسية', 'en'    => 'Main'], 'route' => 'index', 'module' => 'index', 'as' => 'index', 'icon' => 'fa fa-home', 'parent' => '0', 'parent_original' => '0', 'sidebar_link' => '1', 'appear' => '1', 'ordering' => '1']);
+        $manageMain->parent_show = $manageMain->id;
+        $manageMain->save();
+
+        //===== Packages =====
+        //Packages / packages
+        $managePackages = Permission::create(['name' => 'manage_packages', 'display_name' => ['ar'  =>  'إدارة الطرود',  'en'    =>  'Manage Packages'], 'route' => 'packages', 'module' => 'packages', 'as' => 'packages.index', 'icon' => 'fas fa-boxes', 'parent' => '0', 'parent_original' => '0', 'sidebar_link' => '1', 'appear' => '1', 'ordering' => '5',]);
+        $managePackages->parent_show = $managePackages->id;
+        $managePackages->save();
+        $showPackages    =  Permission::create(['name' => 'show_packages',  'display_name' =>   ['ar'   =>  'الطرود',   'en'        =>  'Packages'], 'route' => 'packages', 'module' => 'packages', 'as' => 'packages.index', 'icon' => 'fas fa-boxes', 'parent' => '0', 'parent_original' => '0', 'parent_show' => '0', 'sidebar_link' => '0', 'appear' => '0']);
+        $createPackages  =  Permission::create(['name' => 'create_packages', 'display_name'  =>   ['ar'   =>  'إضافة طرد',   'en'       =>  'Add New Package'], 'route' => 'packages', 'module' => 'packages', 'as' => 'packages.create', 'icon' => null, 'parent' => '0', 'parent_original' => '0', 'parent_show' => '0', 'sidebar_link' => '0', 'appear' => '0']);
+        $displayPackages =  Permission::create(['name' => 'display_packages', 'display_name'  =>   ['ar'   =>  'استعراض طرد',   'en'      =>  'Display Package'], 'route' => 'packages', 'module' => 'packages', 'as' => 'packages.show', 'icon' => null, 'parent' => '0', 'parent_original' => '0', 'parent_show' => '0', 'sidebar_link' => '0', 'appear' => '0']);
+        $updatePackages  =  Permission::create(['name' => 'update_packages', 'display_name'  =>   ['ar'   =>  'تعديل طرد',   'en'        =>  'Update Package'], 'route' => 'packages', 'module' => 'packages', 'as' => 'packages.edit', 'icon' => null, 'parent' => '0', 'parent_original' => '0', 'parent_show' => '0', 'sidebar_link' => '0', 'appear' => '0']);
+        $deletePackages  =  Permission::create(['name' => 'delete_packages', 'display_name'  =>   ['ar'   =>  'حذف طرد',   'en'          =>  'Delete Package'], 'route' => 'packages', 'module' => 'packages', 'as' => 'packages.destroy', 'icon' => null, 'parent' => '0', 'parent_original' => '0', 'parent_show' => '0', 'sidebar_link' => '0', 'appear' => '0']);
+
+
         // ===== Products =====
         $manageProducts = Permission::create(['name' => 'merchant_manage_products',  'display_name' => ['ar' => 'إدارة المنتجات', 'en' => 'Manage Products'], 'route' => 'products', 'module' => 'products', 'as' => 'products.index', 'icon' => 'fab fa-product-hunt', 'parent' => '0', 'parent_original' => '0', 'sidebar_link'    => '1', 'appear' => '1', 'ordering' => '10',]);
         $manageProducts->parent_show = $manageProducts->id;
@@ -34,6 +51,16 @@ class MerchantPermissionsSeeder extends Seeder
 
         // ربط الصلاحيات بدور التاجر
         $permissions = [
+            // Dashboard
+            $manageMain,
+            // Packages
+            $managePackages,
+            $showPackages,
+            $createPackages,
+            $displayPackages,
+            $updatePackages,
+            $deletePackages,
+            // Products
             $manageProducts,
             $showProducts,
             $createProducts,
