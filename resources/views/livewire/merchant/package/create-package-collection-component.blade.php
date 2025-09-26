51fc8cd5d5cdef37d58bd774ca7c0327619703d6
khaleelraweh <khaleelvisa@gmail.com>
2025-09-26 21:45:47 +0300

    save all

--- resources/views/livewire/merchant/package/create-package-collection-component.blade.php
+++ resources/views/livewire/merchant/package/create-package-collection-component.blade.php
@@ -48,12 +48,14 @@
 
                 <div class="col-sm-4 mb-3">
                     <label class="col-form-label">{{ __('package.insurance_fee') }}</label>
-                    <input wire:model="insurance_fee" name="insurance_fee" id="insurance_fee" type="number" step="0.01" class="form-control">
+                    <p class="mb-0">{{ $insurance_fee }}</p>
+                    <input wire:model="insurance_fee" name="insurance_fee" id="insurance_fee" type="hidden" step="0.01" class="form-control">
                 </div>
 
                 <div class="col-sm-4 mb-3">
                     <label class="col-form-label">{{ __('package.service_fee') }}</label>
-                    <input wire:model="service_fee" name="service_fee" id="service_fee" type="number" step="0.01" class="form-control">
+                    <p class="mb-0">{{ $service_fee }}</p>
+                    <input wire:model="service_fee" name="service_fee" id="service_fee" type="hidden" step="0.01" class="form-control">
                 </div>
             </div>
 
@@ -61,17 +63,20 @@
             <div class="row">
                 <div class="col-sm-4 mb-3">
                     <label class="col-form-label">{{ __('package.total_fee') }}</label>
-                    <input type="number" name="total_fee" id="total_fee" class="form-control" value="{{ $this->totalFee }}" readonly>
+                    <p class="mb-0">{{ $this->totalFee }}</p>
+                    <input type="hidden" name="total_fee" id="totalFee" class="form-control" value="{{ $this->totalFee }}" readonly>
                 </div>
 
                 <div class="col-sm-4 mb-3">
                     <label class="col-form-label">{{ __('package.paid_amount') }}</label>
-                    <input wire:model="paid_amount" name="paid_amount" id="paid_amount" type="number" step="0.01" class="form-control">
+                    <p class="mb-0">{{ $paid_amount }}</p>
+                    <input wire:model="paid_amount" name="paid_amount" id="paid_amount" type="hidden" step="0.01" class="form-control">
                 </div>
 
                 <div class="col-sm-4 mb-3">
                     <label class="col-form-label">{{ __('package.remaining_amount') }}</label>
-                    <input type="number" name="due_amount" id="due_amount" class="form-control" value="{{ $this->remainingAmount }}" readonly>
+                    <p class="mb-0">{{ $this->remainingAmount }}</p>
+                    <input type="hidden" name="due_amount" id="due_amount" class="form-control" value="{{ $this->remainingAmount }}" readonly>
                 </div>
             </div>
 
