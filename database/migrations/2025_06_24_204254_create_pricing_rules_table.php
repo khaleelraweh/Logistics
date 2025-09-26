ab60a2992f041c15f5bceb39f8786d2a8b4714c0
khaleelraweh <khaleelvisa@gmail.com>
2025-09-27 01:18:59 +0300

    save all

--- database/migrations/2025_06_24_204254_create_pricing_rules_table.php
+++ database/migrations/2025_06_24_204254_create_pricing_rules_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
         Schema::create('pricing_rules', function (Blueprint $table) {
             $table->id();
             $table->json('name');
+            $table->json('slug');
             $table->json('description')->nullable();
             $table->enum('type', ['delivery', 'storage', 'handling'])->default('delivery');
             $table->string('zone')->nullable(); // المدينة أو المنطقة
