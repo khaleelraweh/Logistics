547853bbaf9d26b8ac943755a7ff1c036e0d88ab
khaleelraweh <khaleelvisa@gmail.com>
2025-09-27 01:03:18 +0300

    save all

--- app/Models/PricingRule.php
+++ app/Models/PricingRule.php
@@ -2,21 +2,22 @@
 
 namespace App\Models;
 
+use App\Helper\MySlugHelper;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Spatie\Translatable\HasTranslations;
 use Nicolaslopezj\Searchable\SearchableTrait;
+use Spatie\Sluggable\SlugOptions;
 
 class PricingRule extends Model
 {
-    use HasFactory , HasTranslations;
+    use HasFactory , HasTranslations , SearchableTrait;
 
     protected $guarded = [];
     public $translatable = ['name','description'];
 
        // Cast fields
     protected $casts = [
-        'condition' => 'array',
         'base_price' => 'decimal:2',
         'price_per_kg' => 'decimal:2',
         'published_on' => 'datetime',
@@ -30,6 +31,41 @@ class PricingRule extends Model
             'pricing_rules.type' => 2,
         ],
     ];
+
+    public function getSlugOptions(): SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('name')
+            ->saveSlugsTo('slug');
+    }
+
+    protected function generateNonUniqueSlug(): string
+    {
+        $slugField = $this->slugOptions->slugField;
+        $slugString = $this->getSlugSourceString();
+
+        $slug = $this->getTranslations($slugField)[$this->getLocale()] ?? null;
+
+        $slugGeneratedFromCallable = is_callable($this->slugOptions->generateSlugFrom);
+        $hasCustomSlug = $this->hasCustomSlugBeenUsed() && !empty($slug);
+        $hasNonChangedCustomSlug = !$slugGeneratedFromCallable && !empty($slug) && !$this->slugIsBasedOnTitle();
+
+        if ($hasCustomSlug || $hasNonChangedCustomSlug) {
+            $slugString = $slug;
+        }
+
+        return MySlugHelper::slug($slugString);
+    }
+
+    protected function asJson($value)
+    {
+        return json_encode($value, JSON_UNESCAPED_UNICODE);
+    }
+
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
 
 
     // Example Accessor: full display name
