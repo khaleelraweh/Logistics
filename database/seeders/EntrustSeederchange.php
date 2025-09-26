e402d8be64ab26138e6dd544b22943447a47d418
khaleelraweh <khaleelvisa@gmail.com>
2025-09-26 22:46:12 +0300

    save all

--- database/seeders/EntrustSeederchange.php
+++ database/seeders/EntrustSeederchange.php
@@ -27,16 +27,16 @@ class EntrustSeeder extends Seeder
     public function run()
     {
 
-        //Invoices // invoices
+        //PricingRules // pricing_rules
 
-        $manageInvoices = Permission::create(['name' => 'manage_invoices', 'display_name' => ['ar'  =>  'إدارة الفواتير',  'en'    =>  'Manage Invoices'], 'route' => 'invoices', 'module' => 'invoices', 'as' => 'invoices.index', 'icon' => 'fas fa-external-link-alt', 'parent' => '0', 'parent_original' => '0', 'sidebar_link' => '1', 'appear' => '1', 'ordering' => '130',]);
-        $manageInvoices->parent_show = $manageInvoices->id;
-        $manageInvoices->save();
-        $showInvoices    =  Permission::create(['name' => 'show_invoices',  'display_name' =>   ['ar'   =>  'الفواتير',   'en'        =>  'Invoices'], 'route' => 'invoices', 'module' => 'invoices', 'as' => 'invoices.index', 'icon' => 'fas fa-external-link-alt', 'parent' => '0', 'parent_original' => '0', 'parent_show' => '0', 'sidebar_link' => '0', 'appear' => '0']);
-        $createInvoices  =  Permission::create(['name' => 'create_invoices', 'display_name'  =>   ['ar'   =>  'إضافة فاتورة',   'en'       =>  'Add New Invoice'], 'route' => 'invoices', 'module' => 'invoices', 'as' => 'invoices.create', 'icon' => null, 'parent' => '0', 'parent_original' => '0', 'parent_show' => '0', 'sidebar_link' => '0', 'appear' => '0']);
-        $displayInvoices =  Permission::create(['name' => 'display_invoices', 'display_name'  =>   ['ar'   =>  'استعراض  فاتورة',   'en'      =>  'Display Invoice'], 'route' => 'invoices', 'module' => 'invoices', 'as' => 'invoices.show', 'icon' => null, 'parent' => '0', 'parent_original' => '0', 'parent_show' => '0', 'sidebar_link' => '0', 'appear' => '0']);
-        $updateInvoices  =  Permission::create(['name' => 'update_invoices', 'display_name'  =>   ['ar'   =>  'تعديل فاتورة',   'en'        =>  'Update Invoice'], 'route' => 'invoices', 'module' => 'invoices', 'as' => 'invoices.edit', 'icon' => null, 'parent' => '0', 'parent_original' => '0', 'parent_show' => '0', 'sidebar_link' => '0', 'appear' => '0']);
-        $deleteInvoices  =  Permission::create(['name' => 'delete_invoices', 'display_name'  =>   ['ar'   =>  'حذف فاتورة',   'en'          =>  'Delete Invoice'], 'route' => 'invoices', 'module' => 'invoices', 'as' => 'invoices.destroy', 'icon' => null, 'parent' => '0', 'parent_original' => '0', 'parent_show' => '0', 'sidebar_link' => '0', 'appear' => '0']);
+        $managePricingRules = Permission::create(['name' => 'manage_pricing_rules', 'display_name' => ['ar'  =>  'إدارة الفواتير',  'en'    =>  'Manage Invoices'], 'route' => 'invoices', 'module' => 'invoices', 'as' => 'invoices.index', 'icon' => 'fas fa-external-link-alt', 'parent' => '0', 'parent_original' => '0', 'sidebar_link' => '1', 'appear' => '1', 'ordering' => '130',]);
+        $managePricingRules->parent_show = $managePricingRules->id;
+        $managePricingRules->save();
+        $showPricingRules    =  Permission::create(['name' => 'show_pricing_rules',  'display_name' =>   ['ar'   =>  'الفواتير',   'en'        =>  'Invoices'], 'route' => 'invoices', 'module' => 'invoices', 'as' => 'invoices.index', 'icon' => 'fas fa-external-link-alt', 'parent' => '0', 'parent_original' => '0', 'parent_show' => '0', 'sidebar_link' => '0', 'appear' => '0']);
+        $createPricingRules  =  Permission::create(['name' => 'create_pricing_rules', 'display_name'  =>   ['ar'   =>  'إضافة فاتورة',   'en'       =>  'Add New Invoice'], 'route' => 'invoices', 'module' => 'invoices', 'as' => 'invoices.create', 'icon' => null, 'parent' => '0', 'parent_original' => '0', 'parent_show' => '0', 'sidebar_link' => '0', 'appear' => '0']);
+        $displayPricingRules =  Permission::create(['name' => 'display_pricing_rules', 'display_name'  =>   ['ar'   =>  'استعراض  فاتورة',   'en'      =>  'Display Invoice'], 'route' => 'invoices', 'module' => 'invoices', 'as' => 'invoices.show', 'icon' => null, 'parent' => '0', 'parent_original' => '0', 'parent_show' => '0', 'sidebar_link' => '0', 'appear' => '0']);
+        $updatePricingRules  =  Permission::create(['name' => 'update_pricing_rules', 'display_name'  =>   ['ar'   =>  'تعديل فاتورة',   'en'        =>  'Update Invoice'], 'route' => 'invoices', 'module' => 'invoices', 'as' => 'invoices.edit', 'icon' => null, 'parent' => '0', 'parent_original' => '0', 'parent_show' => '0', 'sidebar_link' => '0', 'appear' => '0']);
+        $deletePricingRules  =  Permission::create(['name' => 'delete_pricing_rules', 'display_name'  =>   ['ar'   =>  'حذف فاتورة',   'en'          =>  'Delete Invoice'], 'route' => 'invoices', 'module' => 'invoices', 'as' => 'invoices.destroy', 'icon' => null, 'parent' => '0', 'parent_original' => '0', 'parent_show' => '0', 'sidebar_link' => '0', 'appear' => '0']);
 
     }
 }
