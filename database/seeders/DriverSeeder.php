d1f1a112fad68714ab8c96fe3105f6e1a5d726bf
khaleelraweh <khaleelvisa@gmail.com>
2025-09-29 22:18:32 +0300

    save all

--- database/seeders/DriverSeeder.php
+++ database/seeders/DriverSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use App\Models\Driver;
+use App\Models\Permission;
+use App\Models\Role;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 
@@ -10,7 +13,7 @@ class DriverSeeder extends Seeder
 {
     public function run()
     {
-        Driver::create([
+        $driver = Driver::create([
             'first_name' => ['ar' => 'خالد', 'en' => 'Khaled'],
             'middle_name' => ['ar' => 'احمد', 'en' => 'Ahmed'],
             'last_name' => ['ar' => 'حمود', 'en' => 'Hammoud'],
@@ -47,5 +50,52 @@ class DriverSeeder extends Seeder
             'created_by' => 'system',
             'updated_by' => null,
         ]);
+
+
+        // 2- add user account to driver
+        $driverUser = User::create([
+            'first_name' => $driver->name,
+            'last_name' => $driver->contact_person,
+            'username' => 'alnagah',
+            'email' => $driver->email,
+            'mobile' => $driver->phone,
+            'password'      => bcrypt('123123123'),
+            'layout_preferences'    => json_encode([
+                                        "layout"    => "vertical",
+                                        "topbar"    => "dark",
+                                        "sidebar"   => "dark",
+                                        "sidebar_size"  => "default",
+                                        "layout_size"   => "fluid",
+                                        "preloader" => true,
+                                        "rtl"   => true,
+                                        "mode"  => "light",
+                                        "locale" => session('locale', config('locales.fallback_locale')),
+                                    ]),
+            'status' => 1,
+            'created_by' => 'Seeder',
+        ]);
+
+        // 3- connect driver account to user account
+        $driver->update(['user_id' => $driverUser->id]);
+
+
+        // 4- connect user account of the driver to the role driver
+        $driverRole = Role::where('name', 'driver')->first();
+
+        if (!$driverUser->hasRole($driverRole->name)) {
+            $driverUser->attachRole($driverRole);
+        }
+
+        // 5- add driver permission to the driver user
+        $permissions = Permission::where('name', 'like', 'driver_%')->get();
+        foreach ($permissions as $permission) {
+            if (!$driverRole->hasPermission($permission->name)) {
+                $driverRole->attachPermission($permission);
+            }
+        }
+
+        // 6- gave a message of successfull
+        $this->command->info('تم إنشاء السائق وربطه بالدور والصلاحيات في جدول users بنجاح!');
+
     }
 }
