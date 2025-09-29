c10d4c38b31224cc20ed45fe75841dac31625c10
khaleelraweh <khaleelvisa@gmail.com>
2025-09-30 00:02:09 +0300

    save all

--- routes/driver.php
+++ routes/driver.php
@@ -14,6 +14,18 @@ Route::group(['prefix' => 'driver', 'as' => 'driver.', 'middleware' => ['roles',
     Route::get('/lock-screen', [DriverDashboardController::class, 'lock_screen'])->name('lock-screen');
     Route::post('/unlock', [DriverDashboardController::class, 'unlock'])->name('unlock');
 
-    // Deliveries
+
+    // Profile & Settings
+    Route::get('layout-customizer', [DriverDashboardController::class, 'layoutCustomizer'])->name('profile.layout-customizer');
+    Route::post('/user/layout-preferences', [DriverDashboardController::class, 'updateModeFromRightBar'])->name('profile.updateModeFromRightBar');
+    Route::post('/update-language-preference', [DriverDashboardController::class, 'updateLanguagePreference'])->name('profile.updatelanguagepreference')->middleware(['auth']);
+
+    // Profile Management
+    Route::get('/profile', [DriverDashboardController::class, 'profile'])->name('profile');
+    Route::post('driver/profile/remove-image', [DriverDashboardController::class, 'remove_image'])->name('profile.remove_image');
+    Route::patch('/profile', [DriverDashboardController::class, 'update_profile'])->name('profile.update');
+
+
+    //Resources
     Route::resource('deliveries', DriverDeliveryController::class);
 });
