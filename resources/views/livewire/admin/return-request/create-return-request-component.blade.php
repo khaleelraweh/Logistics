230de2f7eb2510a4d4d111fb4f07f592e664c928
khaleelraweh <khaleelvisa@gmail.com>
2025-09-30 23:38:00 +0300

    save all

--- resources/views/livewire/admin/return-request/create-return-request-component.blade.php
+++ resources/views/livewire/admin/return-request/create-return-request-component.blade.php
@@ -289,8 +289,8 @@
                                 <div class="col-sm-10">
                                     <select wire:model="status" class="form-select">
                                          <option value="requested">{{ __('return_request.status_requested') }}</option>
-                                         <option value="requested">{{ __('return_request.status_assigned_to_driver') }}</option>
-                                         <option value="requested">{{ __('return_request.status_picked_up') }}</option>
+                                         <option value="assigned_to_driver">{{ __('return_request.status_assigned_to_driver') }}</option>
+                                         <option value="picked_up">{{ __('return_request.status_picked_up') }}</option>
                                         <option value="cancelled">{{ __('return_request.status_cancelled') }}</option>
                                         <option value="in_transit">{{ __('return_request.status_in_transit') }}</option>
                                         <option value="rejected">{{ __('return_request.status_rejected') }}</option>
