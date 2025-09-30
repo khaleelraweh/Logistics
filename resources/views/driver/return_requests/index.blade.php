c2e97f5edc87a75ce0b9f2220144ffb94cfb7e28
khaleelraweh <khaleelvisa@gmail.com>
2025-10-01 00:25:39 +0300

    save all

--- resources/views/driver/return_requests/index.blade.php
+++ resources/views/driver/return_requests/index.blade.php
@@ -161,6 +161,7 @@
                                                 </li>
                                                 @endability
 
+
                                             </ul>
                                         </div>
                                     </td>
