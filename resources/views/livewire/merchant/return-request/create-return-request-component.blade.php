2f04b375251cecf6c17c850ea41ccfd718fe39e5
khaleelraweh <khaleelvisa@gmail.com>
2025-10-01 22:17:30 +0300

    save all

--- resources/views/livewire/merchant/return-request/create-return-request-component.blade.php
+++ resources/views/livewire/merchant/return-request/create-return-request-component.blade.php
@@ -312,7 +312,7 @@
 
                             <!-- Submit Button -->
                             <div class="text-end pt-3">
-                                @ability('admin', 'create_return_requests')
+                                @ability('merchant', 'merchant_create_return_requests')
                                     <button type="submit" class="btn btn-primary px-3 d-inline-flex align-items-center">
                                         <i class="ri-save-3-line me-2"></i>
                                         <i class="bi bi-save me-2"></i>
@@ -320,7 +320,7 @@
                                     </button>
                                 @endability
 
-                                <a href="{{ route('admin.return_requests.index') }}" class="btn btn-outline-danger ms-2">
+                                <a href="{{ route('merchant.return_requests.index') }}" class="btn btn-outline-danger ms-2">
                                     <i class="ri-arrow-go-back-line me-1"></i>
                                     {{ __('panel.cancel') }}
                                 </a>
