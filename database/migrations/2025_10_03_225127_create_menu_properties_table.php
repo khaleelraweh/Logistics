19e495ccad7feec3626323632bd7ae3f54b587dd
khaleelraweh <khaleelvisa@gmail.com>
2025-10-04 01:55:56 +0300

    save all

--- database/migrations/2025_10_03_225127_create_menu_properties_table.php
+++ database/migrations/2025_10_03_225127_create_menu_properties_table.php
@@ -15,7 +15,18 @@ return new class extends Migration
     {
         Schema::create('menu_properties', function (Blueprint $table) {
             $table->id();
+
+            $table->unsignedBigInteger('menu_id'); // الوحدة التي تنتمي لها الخاصية
+            $table->json('property_value'); // قيمة الخاصية (يمكن أن تكون متعددة اللغات)
+
+            $table->boolean('status')->default(true);
+            $table->string('created_by')->nullable();
+            $table->string('updated_by')->nullable();
+            $table->dateTime('published_on')->nullable();
+            $table->softDeletes();
             $table->timestamps();
+
+            $table->foreign('menu_id')->references('id')->on('menus')->onDelete('cascade');
         });
     }
 
