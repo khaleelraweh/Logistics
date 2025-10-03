f26f81bda3c982e97f89565aa213164b0a6dfdd9
khaleelraweh <khaleelvisa@gmail.com>
2025-10-04 01:57:34 +0300

    save all

--- database/seeders/MenuPropertySeeder.php
+++ database/seeders/MenuPropertySeeder.php
@@ -2,18 +2,38 @@
 
 namespace Database\Seeders;
 
+use App\Models\Menu;
+use App\Models\MenuProperty;
+use Faker\Factory;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class MenuPropertySeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
     public function run()
     {
-        //
+        $faker = Factory::create();
+
+        // جلب جميع وحدات النظام
+        $modules = Menu::where('section', 3)->get();
+
+        $propertiesData = [
+            ['ar' => 'الإرجاع عبر قنوات متعددة', 'en' => 'Multi-channel Returns'],
+            ['ar' => 'تجميع المرتجعات', 'en' => 'Returns Consolidation'],
+            ['ar' => 'التتبع الحي', 'en' => 'Live Tracking'],
+            ['ar' => 'التسوية المالية', 'en' => 'Financial Settlement'],
+        ];
+
+        foreach ($modules as $module) {
+            foreach ($propertiesData as $prop) {
+                MenuProperty::create([
+                    'menu_id'        => $module->id,
+                    'property_value' => $prop,
+                    'created_by'     => 'admin',
+                    'status'         => true,
+                    'published_on'   => $faker->dateTimeBetween('-6 months', 'now'),
+                ]);
+            }
+        }
     }
 }
