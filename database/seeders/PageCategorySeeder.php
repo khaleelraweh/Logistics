2e235f220a80c6bb96ee82d83b488af4fd3c58ee
khaleelraweh <khaleelvisa@gmail.com>
2025-10-03 23:19:44 +0300

    save all

--- database/seeders/PageCategorySeeder.php
+++ database/seeders/PageCategorySeeder.php
@@ -4,7 +4,6 @@ namespace Database\Seeders;
 
 use App\Models\PageCategory;
 use Faker\Factory;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Carbon;
 
@@ -12,24 +11,44 @@ class PageCategorySeeder extends Seeder
 {
     /**
      * Run the database seeds.
-     *
-     * @return void
      */
-    public function run()
+    public function run(): void
     {
         $faker = Factory::create();
 
-        $page = PageCategory::create([
-            'title' => ['ar' => 'العنوان', 'en' => 'title'],
-            'content' => ['ar' => $faker->realText(50), 'en' => $faker->realText(50)],
+        $categories = [
+            ['ar' => 'خدماتنا', 'en' => 'Our Services'],
+            ['ar' => 'حول الشركة', 'en' => 'About Us'],
+            ['ar' => 'الأسئلة الشائعة', 'en' => 'FAQ'],
+            ['ar' => 'سياسة الخصوصية', 'en' => 'Privacy Policy'],
+            ['ar' => 'الشروط والأحكام', 'en' => 'Terms & Conditions'],
+        ];
 
-            'metadata_title' => ['ar' => $faker->realText(50), 'en' => $faker->realText(50)],
-            'metadata_description' => ['ar' => $faker->realText(50), 'en' => $faker->realText(50)],
-            'metadata_keywords' => ['ar' => $faker->realText(50), 'en' => $faker->realText(50)],
-
-            'status' => true,
-            'created_by' => $faker->realTextBetween(10, 20),
-            'updated_by' => $faker->realTextBetween(10, 20), // Assuming you want this as well
-        ]);
+        foreach ($categories as $category) {
+            PageCategory::create([
+                'title' => $category,
+                'content' => [
+                    'ar' => $faker->realText(100),
+                    'en' => $faker->realText(100),
+                ],
+                'metadata_title' => [
+                    'ar' => $faker->sentence(6),
+                    'en' => $faker->sentence(6),
+                ],
+                'metadata_description' => [
+                    'ar' => $faker->realText(150),
+                    'en' => $faker->realText(150),
+                ],
+                'metadata_keywords' => [
+                    'ar' => implode(',', $faker->words(5)),
+                    'en' => implode(',', $faker->words(5)),
+                ],
+                'status' => true,
+                'created_by' => 1, // ID افتراضي للمستخدم (ممكن تغيره)
+                'updated_by' => 1,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ]);
+        }
     }
 }
